d832e8063629aaa467680c4a46f53175b0949d76
teotronic5438 <elias.orihuela>
2024-12-11 23:49:16 -0300

    probando eventos y menu hamburguesa

---
+++ js/dom.js
@@ -0,0 +1,14 @@
+boton.addEventListener("click", function(){
+    menu.classList.toggle("mostrar");
+    console.log("entre a la funcion");
+})
+
+// const header = document.querySelector("header");
+
+// // header.onclick = function(){
+// //     alert("Has clickeado")
+// // }
+
+// // La otra opcion mas usada, puede ejecutarse mas de un evento
+// header.addEventListener("click", () => alert("Hiciste el primer click"))
+// header.addEventListener("click", () => alert("Hiciste el 2do  click"))
